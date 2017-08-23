c4fc8f2329010d2f89d990a414d341d2f97cd57d
Zafer Sevik <zafersevik@gmail.com>
2017-08-23 16:19:24 +0300

    Merge Sort improved

--- MergeSort.playground/Contents.swift
+++ MergeSort.playground/Contents.swift
@@ -1,48 +1,52 @@
 import Foundation
 
 func merge<T: Comparable>(_ array: inout [T], _ left: Int, _ middle: Int, _ right: Int) {
+    let leftSubarray = Array(array[left...middle])
+    let rightSubarray = Array(array[(middle+1)...right])
     
-    let leftSide = Array(array[left...middle])
-    let rightSide = Array(array[(middle+1)...right])
-    
-    var k = left
+    var index = left
     var leftIndex = 0
     var rightIndex = 0
     
-    while (leftIndex < leftSide.count) && (rightIndex < rightSide.count) {
-        if leftSide[leftIndex] < rightSide[rightIndex] {
-            array[k] = leftSide[leftIndex]
+    while (leftIndex < leftSubarray.count) && (rightIndex < rightSubarray.count) {
+        if leftSubarray[leftIndex] < rightSubarray[rightIndex] {
+            array[index] = leftSubarray[leftIndex]
             leftIndex += 1
         }
         else {
-            array[k] = rightSide[rightIndex]
+            array[index] = rightSubarray[rightIndex]
             rightIndex += 1
         }
-        k += 1
+        index += 1
     }
     
-    while leftIndex < leftSide.count {
-        array[k] = leftSide[leftIndex]
-        k += 1
+    while leftIndex < leftSubarray.count {
+        array[index] = leftSubarray[leftIndex]
         leftIndex += 1
+        index += 1
     }
     
-    while rightIndex < rightSide.count {
-        array[k] = rightSide[rightIndex]
-        k += 1
+    while rightIndex < rightSubarray.count {
+        array[index] = rightSubarray[rightIndex]
         rightIndex += 1
+        index += 1
     }
 }
 
 func mergeSort<T: Comparable>(_ array: inout [T], _ left: Int, _ right: Int) {
-    let q = (left + right) / 2 // midway between left and right
-    
-    if left < right {
-        mergeSort(&array, left, q)
-        mergeSort(&array, q+1, right)
-        merge(&array, left, q, right)
+    if left >= right {
+        return
     }
+    
+    let middle = (left + right) / 2
+    
+    mergeSort(&array, left, middle)
+    mergeSort(&array, middle+1, right)
+    merge(&array, left, middle, right)
 }
 
 var numbers = [13, 77, 20, 45, 2, 15, 0, 59, 5, 68, 51, 1, -1, 77]
 mergeSort(&numbers, 0, numbers.count - 1)
+
+
+
